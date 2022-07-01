d25c75c6bc692fe103cac3971d8c80384722f402
Norbert-OO <norbertodongoo@gmail.com>
2022-06-30 19:32:28 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- program entry point
+ *
+ * Description: 'Program points out importance of proper grammar'
+ *
+ * Return: 0 for sucess
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
