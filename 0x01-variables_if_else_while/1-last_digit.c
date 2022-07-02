3dad7345dde5336c6717c99a01986f1841247e88
Norbert-OO <norbertodongoo@gmail.com>
2022-07-02 15:16:57 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,18 +21,18 @@ srand(time(0));
 
 n = rand() - RAND_MAX / 2;
 
-printf("last digit of %d is ",n);
+printf("last digit of %d is",n);
 if (n > 5)
 {
-printf("and is greater than 5");
+printf(" and is greater than 5");
 }
 if (n == 0)
 {
-printf("and is 0");
+printf(" and is 0");
 }
 if (n < 0 && n != 0)
 {
-printf("and is less than 6 and not 0");
+printf(" and is less than 6 and not 0");
 }
 printf("\n");
 
