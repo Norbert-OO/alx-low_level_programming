5017b9ad954d4a28b8b050250c8104b89111d67b
Norbert-OO <norbertodongoo@gmail.com>
2022-07-03 06:57:00 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'A program that prints the alphabet
+ * in lowercase, followed by a new line.'
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a == 'q' || a == 'e')
+continue;
+}
+putchar (a);
+return (0);
+}
