2ae4e84762ba402f0808491aaceb6bd6fb45d4fc
Norbert-OO <norbertodongoo@gmail.com>
2022-07-10 13:42:41 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - A function that adds two integers and returns the result.
+ * @x: parameter1
+ * @y: parameter2
+ * Return: x + y
+ */
+
+int add(int x, int y)
+{
+return (x + y);
+}
