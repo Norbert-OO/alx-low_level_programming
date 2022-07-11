0e0efc20bf997c5704d95a67cf046390a0a8a50b
Norbert-OO <norbertodongoo@gmail.com>
2022-07-11 13:48:28 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,18 +16,18 @@ int _atoi(char *s)
 	temp = s;
 	num = 0;
 	sign = 1;
-	while (*tmp != '\0' && (*temp < '0' || *temp > '9'))
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-		if (*temp == ('-')
+		if (*temp == '-')
 			sign *= -1;
-	temp++;
-}
-if (*temp != '\0')
-{
+		temp++;
+	}
+	if (*temp != '\0')
+	{
 	do {
 		num = num * 10 + (*temp - '0');
 		temp++;
 	} while (*temp >= '0' && *temp <= '9');
-}
+	}
 return (num * sign);
 }
