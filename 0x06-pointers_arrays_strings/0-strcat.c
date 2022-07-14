7383c525999d78afad619046d3ad328484d41d9e
Norbert-OO <norbertodongoo@gmail.com>
2022-07-14 06:42:22 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string>
+
+/**
+ * _strcat - A  function that concatenates two strings.
+ * @dest: 1st Parameter
+ * @src: 2nd Parameter
+ * Return: String
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
