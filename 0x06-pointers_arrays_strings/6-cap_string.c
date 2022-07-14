322eeb935c1db5acea174e58bf7ce223a19cf742
Norbert-OO <norbertodongoo@gmail.com>
2022-07-14 14:02:58 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,33 +3,26 @@
 /**
  * cap_string - A function that
  * capitalizes all words of a string.
- *
- * @str: String.
+ * @x: String.
  * Return: The Capitalized string
  *
  */
-char *cap_string(char *str)
+char *cap_string(char *x)
 {
-	int index = 0;
+char spc[] = {32, 9, '\n', ',' ';' '.', '!', '?' '"', '(' ')' '{' '}'};
+int len = 13;
+int a = 0, i;
 
-	while (str[++index])
-	{
-	while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++;
-	if (str[index - 1] == ' ' ||
-			str[index - 1] == '\t' ||
-			str[index - 1] == '\n' ||
-			str[index - 1] == ',' ||
-			str[index - 1] == ';' ||
-			str[index - 1] == '.' ||
-			str[index - 1] == '!' ||
-			str[index - 1] == '?' ||
-			str[index - 1] == '"' ||
-			str[index - 1] == '(' ||
-			str[index - 1] == ')' ||
-			str[index - 1] == '{' ||
-			str[index - 1] == '}')
-		str[index] -= 32;
-	}
-	return (str);
+while (x[a])
+{
+i = 0;
+while (i < len)
+{
+if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+x[a] = x[a] - 32;
+i++;
+}
+a++;
+}
+return (x);
 }
