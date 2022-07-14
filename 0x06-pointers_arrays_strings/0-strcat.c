61bc5d5fb015d4092cb9b3afe4e11d3bedfbb58d
Norbert-OO <norbertodongoo@gmail.com>
2022-07-14 06:56:27 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,22 @@
 #include <string>
 
 /**
- * _strcat - A  function that concatenates two strings.
+ * _strcat - A function that concatenates two strings.
+ * concatenates the string pointed by *src to the end
+ * of the string pointed to by *dest
  * @dest: 1st Parameter
  * @src: 2nd Parameter
  * Return: String
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++ = src[index];
+
 	return (dest);
 }
