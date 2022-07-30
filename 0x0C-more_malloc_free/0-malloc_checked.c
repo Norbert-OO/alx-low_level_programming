282ef974f10f0c1b2c06646912675a2302449276
Norbert-OO <norbertodongoo@gmail.com>
2022-07-30 05:41:00 -0700

    Task 0: A function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - A function that allocates memory using malloc.
+ *
+ * @b: size of memory to be allocated
+ *
+ * Return: Pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
